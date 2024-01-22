316353adb45ce4e9adcde78bbf47a311429a247c
RokasV <vezikauskas.r@gmail.com>
2024-01-22 09:34:09 +0200

    init + styled components

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+import './styles/App.css';
+
+const MainTitle = styled.h1`
+  text-transform: uppercase;
+  font-size: 40px;
+  font-weight: 400;
+`
+
+const MyButton = styled.button`
+${(props) => console.log(props)}
+  background: black;
+  color: white;
+  border: 1px solid black;
+  padding: 5px 10px;
+  font-size: 17px;
+  cursor: pointer;
+  border-radius: 5px;
+  &:hover {
+    background: grey
+  }
+  &:active {
+    outline: 1px solid tomato;
+  }
+`
+
+export default function App() {
+  return (
+    <div className='App container'>
+      <MainTitle>Hello, world!</MainTitle>
+      <hr />
+    <MyButton >Click Me</MyButton>
+    <MyButton $outline>Outline btn</MyButton>
+    </div>
+  );
+}
